9c345bd0e4d90a1acdaf79bcc363c20deaa19f78
zachary-o <104291766+zachary-o@users.noreply.github.com>
2023-01-01 23:49:23 +0200

    Add files via upload

---
+++ DOM Quote Generator/app.js
@@ -0,0 +1,43 @@
+let btn = document.querySelector('#new-quote');
+let quote = document.querySelector('.quote');
+let author = document.querySelector('.author');
+
+const quotes = [
+    {
+        quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+        author: "Nelson Mandela"
+    },
+    {
+        quote: "The way to get started is to quit talking and begin doing.",
+        author: "Walt Disney"
+    },
+    {
+        quote: "If life were predictable it would cease to be life, and be without flavor.",
+        author: "Eleanor Roosevelt"
+        
+    },
+    {
+        quote: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, youll never have enough.",
+        author: "Oprah Winfrey"
+    },
+    {
+        quote: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, youll never have enough.",
+        author: "Oprah Winfrey"
+    },
+    {
+        quote: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
+        author: "James Cameron"
+    },
+    {
+        quote: "Life is what happens when you're busy making other plans.",
+        author: "John Lennon"
+    }];
+
+  btn.addEventListener('click', function() {
+    
+    let random = Math.floor(Math.random() * quotes.length);
+
+    quote.innerText = quotes[random].quote;
+    author.innerText = quotes[random].author;
+
+  })  
